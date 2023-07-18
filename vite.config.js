fef629526ce3bd459b5299283a944df261e28a2f
brasiliaamarela <aninhaalmeidav@gmail.com>
2023-07-18 18:04:24 -0300

    link para page

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/todoreactvitedeploy"
+  base: "/todoreactvitedeploy/todo",
+  plugins: [react()]
 })
